c570c1f800fc1a5fa5a70591f0be6aab9f28d1af
laura vanesa <castro.arce@outllok.es>
2022-06-22 12:49:40 -0500

    este es un cambio

--- conversion.java
+++ conversion.java
@@ -1,2 +1,16 @@
+import javax.swing.*;
+
 public class conversion {
+
+    public static  void main (String arg []){
+
+        double pesosColombianos = 1;
+        double bolivares = 00012;
+
+        double Pc = Double.parseDouble(JOptionPane.showInputDialog("por favor ingrese la cantida de pesos colombianos a bolivares"));
+
+        double calcular = Pc * bolivares;
+        JOptionPane.showMessageDialog(null,"la conversion de Pc a bolivares es :" + calcular);
+    }
 }
+
